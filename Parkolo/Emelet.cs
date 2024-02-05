31ad39fbd231130b818493066eeabef67d0184c7
Szent-Györgyi Technikum <szgyvs2020@gmail.com>
2024-02-05 14:04:58 +0100

    Add project files.

---
+++ Parkolo/Emelet.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parkolo
+{
+    internal class Emelet
+    {
+        public List<int> Szektorok { get; set; }
+        public string SzintNeve { get; set; }
+        public int SzintSzama { get; set; }
+
+        public Emelet(string sor) 
+        {
+            this.Szektorok = new List<int>();
+            string[] data = sor.Split("; ");
+            this.SzintNeve = data[0];
+            this.SzintSzama = 0;
+
+            for (int i = 1; i < data.Length; i++) 
+            {
+                this.Szektorok.Add(int.Parse(data[i]));
+                this.SzintSzama++;
+            }
+        }
+
+        public override string ToString() 
+        {
+            string result = $"{SzintNeve, -12}";
+            foreach (var letszam in Szektorok)
+            {
+                result += $"{letszam, -4}";
+            }
+            return result;
+        }
+    }
+}
---
+++ Parkolo/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Parkolo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Emelet> list = new List<Emelet>();
+
+            StreamReader sr = new StreamReader("../../../src/parkolohaz.txt");
+
+            while (!sr.EndOfStream)
+            {
+                list.Add(new Emelet(sr.ReadLine()));
+            }
+            Console.WriteLine("Szint neve| 1.szektor| 2.szektor| 3.szektor| 4.szektor| 5.szektor| 6.szektor");
+
+            for (int i = 0; i < list.Count; i++) { Console.WriteLine($"{i+1} szint {list[i]}"); }
+
+            Console.WriteLine($"8.Feladat {list.MinBy(e => e.Szektorok.Sum()).SzintNeve}");
+
+            Console.WriteLine($"9.Feladat {list.FirstOrDefault(e => e.Szektorok.Contains(0)).SzintSzama}");
+        }
+    }
+}
