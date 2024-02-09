9482c412f8b68a208a05bc92cdd3bca4dc4c5ae6
Avx <93741404+Golf1ABF@users.noreply.github.com>
2024-02-09 09:07:57 +0100

    Kész feladat

--- Parkolo/Program.cs
+++ Parkolo/Program.cs
@@ -26,9 +26,46 @@ namespace Parkolo
 
             Console.WriteLine($"9.Feladat {list.FirstOrDefault(e => e.Szektorok.Contains(0)).SzintSzama}");
 
-            var atlag = list.All(e => e.Szektorok.Average();
 
-            Console.WriteLine($"10.Feladat {list.Where(e => e.Szektorok.Average() ==)}");
+            var atlag = list.Average(e => e.Szektorok.Average());
+            Console.WriteLine($"10.Feladat Átlag: {list.Sum(e => e.Szektorok.Count(i => i == atlag))} Átlag alatti: {list.Sum(e => e.Szektorok.Count(i => i < atlag))} Átlag feletti: {list.Sum(e => e.Szektorok.Count(i => i > atlag))}");
+
+            StreamWriter sw = new StreamWriter("../../../src/11feladat.txt");
+            sw.WriteLine("11. Feladat");
+            foreach (var item in list) 
+            {
+                foreach (var item1 in item.Szektorok)
+                {
+                    if (item1 == 1)
+                    {
+                        sw.WriteLine($"Emelet: {list.IndexOf(item) + 1} Szektor:    {item.Szektorok.IndexOf(item1) + 1}");
+                    }
+                }
+            }
+            
+            Console.WriteLine("12.Feladat");
+            var maxauto = list.OrderByDescending(e => e.Szektorok.Sum()).First();
+            if (list.IndexOf(maxauto) == list.Count -1)
+            {
+                Console.WriteLine($"Legfelső emeleten van a legtöbb autó. {maxauto}");
+            }
+            else Console.WriteLine("Nem igaz hogy a legfelső emeleten van a legtöbb");
+
+            sw.WriteLine("13.Feladat");
+
+            var osszesszabad = 0;
+            var emelet = 1;
+
+            foreach(var item in list)
+            {
+                var szabadhely = 15 * 6 - item.Szektorok.Sum();
+                sw.WriteLine($"{emelet}. emelet {szabadhely}");
+                emelet++;
+                osszesszabad += szabadhely;
+            }
+            sw.WriteLine($"14.Feladat \n Összesen: {osszesszabad} szabad hely van");
+
+            sw.Close();
         }
     }
 }
